a38d98d52b6c232a6639c9fe3daf648d143536a2
Akash-SmartAnnoyer <smartannoyerakash@gmail.com>
2024-10-13 20:57:27 +0530

    db url updated in Home Component for fixing testing env error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home({ cartIconRef, onItemAdded, searchTerm }) {
     const orgId = localStorage.getItem('orgId');  // Retrieve the orgId from localStorage
     
     if (orgId) {
-      fetch('https://smartserver-json-server.onrender.com/categories')
+      fetch('https://stage-smart-server-default-rtdb.firebaseio.com/categories')
       .then((res) => res.json())
             .then((data) => {
                 const matchedCategories = data.filter(category => category.orgId === parseInt(orgId));  // Filter categories by orgId
@@ -27,7 +27,7 @@ function Home({ cartIconRef, onItemAdded, searchTerm }) {
                 setLoading((prev) => ({ ...prev, categories: false }));
             });
 
-            fetch('https://smartserver-json-server.onrender.com/subcategories')
+            fetch('https://stage-smart-server-default-rtdb.firebaseio.com/subcategories')
             .then((res) => res.json())
             .then((data) => {
                 const matchedSubcategories = data.filter(subcategory => subcategory.orgId === parseInt(orgId));  // Filter subcategories by orgId
@@ -35,7 +35,7 @@ function Home({ cartIconRef, onItemAdded, searchTerm }) {
                 setLoading((prev) => ({ ...prev, subcategories: false }));
             });
 
-            fetch('https://smartserver-json-server.onrender.com/menu_items')
+            fetch('https://stage-smart-server-default-rtdb.firebaseio.com/menu_items')
             .then((res) => res.json())
             .then((data) => {
                 const matchedMenuItems = data.filter(item => item.orgId === parseInt(orgId));  // Filter menu items by orgId
